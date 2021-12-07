d65a2b984068ddcf011c9664dbe634d23d2ebc7c
AnthonyMarc23 <anthony@anthonymarc.me>
2021-12-06 21:01:02 -0500

    remove "portfolio" from index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ export default function Home({ allPostsData }) {
           <p>Hello, I'm Anthony, a UX Designer and front-end web developer. I like to work in Next.js, React, and Vue, and also have some skills with Angular. I have a diverse set of skills that I have leveled up over the years, with a strong focus in UX Design and Game Design, and other notable areas including story telling and writing, music and audio production, world building, and small business ownership. Take a look at some of my projects below!</p>
         </section>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Portfolio</h2>
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, title }) => (
               <li className={utilStyles.listItem} key={id}>
