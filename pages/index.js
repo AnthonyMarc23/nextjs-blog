131a79c06350fff2a9538974fdd4b6a0f10ec8bb
AnthonyMarc23 <anthony@anthonymarc.me>
2022-03-07 17:47:34 -0500

    Updates to index and css, fixes to home screen

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Link from 'next/link';
 import Head from 'next/head';
-// import Image from 'next/image';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-// import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -22,21 +20,22 @@ export default function Home({ allPostsData }) {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section>
-          <p>Hello, I'm Anthony, a Game Designer, UX Designer, front-end web developer. I have a diverse set of skills that I have leveled up over the years, with a strong focus in Game Design and Web Design; other notable areas include storytelling and writing, music and audio production, world building, and small business ownership. My goals are many, but ultimately, leading a design team whether for my own projects or helping somebody else develop their vision makes me the happiest!</p>
-        </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-            </li>
-            ))}
-          </ul>
-        </section>
+        <div className={utilStyles.container}>
+          <section className={utilStyles.about}>
+            <p>Hello, I'm Anthony, a Game Designer, UX Designer, and front-end web developer. I have a diverse set of skills that I have leveled up over the years, with a strong focus in Game Design and Web Design; other notable areas include storytelling and writing, music and audio production, world building, and small business ownership. My goals are many, but ultimately, leading a design team to produce projects of my vision or helping somebody develop their vision makes me the happiest!</p>
+          </section>
+          <section className={`${utilStyles.homeMenu} ${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <ul className={utilStyles.list}>
+              {allPostsData.map(({ id, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <h2><a>{title}</a></h2>
+                </Link>
+              </li>
+              ))}
+            </ul>
+          </section>
+        </div>
       </Layout>
     </>
   )
